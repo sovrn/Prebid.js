6176b2ed52626f97bf5180acdaa5171a66e48857
Ted Rand <randtheodore@gmail.com>
2017-10-19 09:06:09 -0600

    Updated the adapter to hit the new endpoint added to blackbird. Added the initial tests for Prebid. Test for 'interpretResponse' function is still failing.

--- modules/sovrnBidAdapter.js
+++ modules/sovrnBidAdapter.js
@@ -1,122 +1,84 @@
-
-/**
- *  WIP
- */
-
 import * as utils from 'src/utils';
 import { registerBidder } from 'src/adapters/bidderFactory';
 import { BANNER } from 'src/mediaTypes';
-
-const BIDDER_CODE = 'sovrn';
-const URL = '//ap.lijit.com/rtb/bid';
-const USER_PARAMS = ['tagid', 'bidfloor', 'dealid'];
-const SOURCE = 'pbjs';
-var bidid;
+import { REPO_AND_VERSION } from 'src/constants';
 
 export const spec = {
-  code: BIDDER_CODE,
+  code: 'sovrn',
   supportedMediaTypes: [BANNER],
 
+  /**
+   * Check if the bid is a valid zone ID in either number or string form
+   * @param {object} bid the Sovrn bid to validate
+   * @return boolean for whether or not a bid is valid
+   */
   isBidRequestValid: function(bid) {
     return !!(bid.params.tagid && !isNaN(parseFloat(bid.params.tagid)) && isFinite(bid.params.tagid));
   },
 
   /**
-   * Make a server request from the list of BidRequests.
-   *
-   * @param {BidRequest[]} bidRequests A non-empty list of bid requests which should be sent to the Server.
-   * @return ServerRequest Info describing the request to the server.
+   * Format the bid request object for our endpoint
+   * @param {BidRequest[]} bidRequests Array of Sovrn bidders
+   * @return object of parameters for Prebid AJAX request
    */
   buildRequests: function(bidReqs) {
-    const domain = window.location.host;
-    const page = window.location.pathname + location.search + location.hash;
     let sovrnImps = [];
     utils._each(bidReqs, function (bid) {
-      bidid = bid.bidId;
       const tagId = utils.getBidIdParameter('tagid', bid.params);
       const bidFloor = utils.getBidIdParameter('bidfloor', bid.params);
-
-      // BANNER OBJECT NOT USED.
-      // THE WIDTH & HEIGHT ARE OVERRIDDEN BY THE WIDTH AND HEIGHT OF THE TAG.
-      var imp =
-        {
-          id: bid.bidId,
-          banner: {
-            w: 1,
-            h: 1,
-          },
-          tagid: tagId,
-          bidfloor: bidFloor
-        };
+      var imp = {
+        id: bid.bidId,
+        banner: { w: 1, h: 1 },
+        tagid: tagId,
+        bidfloor: bidFloor
+      };
       sovrnImps.push(imp);
     });
     var sovrnBidReq = {
       id: utils.getUniqueIdentifierStr(),
       imp: sovrnImps,
       site: {
-        domain: domain,
-        page: page
+        domain: window.location.host,
+        page: window.location.pathname + location.search + location.hash
       }
     };
     const payloadString = JSON.stringify(sovrnBidReq);
     return {
       method: 'POST',
-      url: URL,
+      url: `//ap.lijit.com/rtb/bid?src=${REPO_AND_VERSION}`,
       data: payloadString,
+      options: {contentType: 'text/plain'}
     };
   },
+
   /**
-   * Unpack the response from the server into a list of bids.
-   *
+   * Format Sovrn responses as Prebid bid responses
    * @param {*} sovrnResponse A successful response from Sovrn.
-   * @return {Bid[]} An array of bids which were nested inside the server.
+   * @return {Bid[]} An array of formatted bids.
   */
-  // const bidResponse = {
-  //     requestId: bidRequest.bidId, // bidObj.placementCode
-  //     bidderCode: spec.code,       // BIDDER_CODE
-  //     cpm: CPM,                    // parseFloat(sovrnBid.price);
-  //     width: WIDTH,                // parseInt(sovrnBid.w);
-  //     height: HEIGHT,              // parseInt(sovrnBid.h);
-  //     creativeId: CREATIVE_ID,     // sovrnBid.id;
-  //     dealId: DEAL_ID,             // sovrnBid.dealid
-  //     currency: CURRENCY,          // "USD"
-  //     netRevenue: true,            // true
-  //     ttl: TIME_TO_LIVE,           // 60?
-  //     referrer: REFERER,           //
-  //     ad: CREATIVE_BODY            // decodeURIComponent(responseAd + responseNurl)
-  // };
   interpretResponse: function(sovrnResponse) {
-    // Array to push the sovrn bids to
     let sovrnBidResponses = [];
 
-    // Not sure which of these are still necessary
-    // since it is only passed successful responses
-    if (sovrnResponse && sovrnResponse.id && sovrnResponse.seatbid && sovrnResponse.seatbid.length !== 0 &&
+    if (sovrnResponse.id && sovrnResponse.seatbid && sovrnResponse.seatbid.length !== 0 &&
       sovrnResponse.seatbid[0].bid && sovrnResponse.seatbid[0].bid.length !== 0) {
       sovrnResponse.seatbid[0].bid.forEach(sovrnBid => {
-        let bidObj = utils.getBidRequest(sovrnBid.impid);
-        if (bidObj && sovrnBid.price && sovrnBid.price !== 0) {
-          const bidResponse = {
-            requestId: bidid,
-            bidderCode: spec.code,
-            cpm: parseFloat(sovrnBid.price),
-            width: parseInt(sovrnBid.w),
-            height: parseInt(sovrnBid.h),
-            creativeId: sovrnBid.id,
-            dealId: sovrnBid.dealid || null,
-            currency: 'USD',
-            netRevenue: true,
-            mediaType: BANNER,
-            // ttl: 60,
-            // referrer: utils.getTopWindowUrl(),
-            // ad: decodeURIComponent(responseAd + responseNurl)
-            ad: decodeURIComponent(`${sovrnBid.adm}<img src=${sovrnBid.nurl}>`)
-          };
-          bidmanager.addBidResponse(bidObj.placementCode, bidResponse);
-          sovrnBidResponses.push(bidResponse);
-        }
-      })
+        sovrnBidResponses.push({
+          requestId: sovrnBid.impid,
+          bidderCode: spec.code,
+          cpm: parseFloat(sovrnBid.price),
+          width: parseInt(sovrnBid.w),
+          height: parseInt(sovrnBid.h),
+          creativeId: sovrnBid.id,
+          dealId: sovrnBid.dealId || null,
+          currency: 'USD',
+          netRevenue: true,
+          mediaType: BANNER,
+          ad: decodeURIComponent(`${sovrnBid.adm}<img src=${sovrnBid.nurl}>`),
+          ttl: 60000
+        });
+      });
     }
+
     return sovrnBidResponses;
   }
 };
