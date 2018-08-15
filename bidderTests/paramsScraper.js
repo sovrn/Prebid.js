d11547ba789c71f0319e0103918cdcc433c498c1
Ankit Prakash <aprakash@sovrn.com>
2018-08-15 15:22:35 -0600

    bidder examples scraper

---
+++ bidderTests/paramsScraper.js
@@ -0,0 +1,76 @@
+const fs = require('fs');
+const readline = require('readline');
+
+const PATH = 'modules/';
+const CODE_BLOCK_PATTERN = /^\s*```[^`]*$/;
+const FILE_NAME_PATTERN = /^modules\/(.*)BidAdapter\.md$/;
+const PROMISES = [];
+const FAILED_FILES = [];
+let allAdUnits = {};
+
+function processFile(file) {
+  return new Promise((resolve, reject) => {
+    const lineReader = readline.createInterface({
+      input: fs.createReadStream(file)
+    });
+
+    let inCodeBlock = false;
+    let foundAdUnits = [];
+    let scriptLines = [];
+
+    lineReader.on('line', line => {
+      const match = line.match(CODE_BLOCK_PATTERN);
+      if(match) {
+        inCodeBlock = !inCodeBlock;
+        if(!inCodeBlock) { // just got done compiling a block of code
+          const adUnits = extractAdUnits(scriptLines);
+          if(adUnits && adUnits.length) {
+            foundAdUnits = foundAdUnits.concat(adUnits);
+          }
+
+          scriptLines = [];
+        }
+      } else if(inCodeBlock) {
+        scriptLines.push(line);
+      }
+    }).on('close', () => {
+      if(foundAdUnits.length) {
+        const bidder = file.match(FILE_NAME_PATTERN)[1];
+        resolve({[bidder]: foundAdUnits});
+      } else {
+        reject(file);
+      }
+
+      lineReader.close();
+    });
+  });
+}
+
+function extractAdUnits(scriptLines) {
+  const script = scriptLines.join("\n");
+  try {
+    eval(script);
+
+    return adUnits; // most of the samples create an array called `adUnits`
+  } catch (e) {
+    // didn't get valid javascript in the code block
+    // console.error("found code block with invalid js: " + e + "\n" + script);
+  }
+}
+
+fs.readdirSync(PATH).filter(f => f.endsWith('.md')).forEach(f => {
+  PROMISES.push(processFile(PATH + f).catch(err => FAILED_FILES.push(err)));
+});
+
+Promise.all(PROMISES)
+  .then((res) => {
+      allAdUnits = Object.assign({}, ...res);
+      console.info("Ad units:");
+      console.log(JSON.stringify(allAdUnits, null, 2));
+      console.error("Failed files:");
+      console.error(FAILED_FILES);
+    })
+  .catch(err => {
+    console.error("Error:");
+    console.error(err);
+  });
