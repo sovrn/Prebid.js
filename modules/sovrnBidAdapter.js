054d6071d6466f01fccb013af4818002e61ef80d
Maxim Pakhomov <maxim_pakhomov@lineate.com>
2022-02-10 19:08:49 +0400

    update test url to fake-rtb-providers

--- modules/sovrnBidAdapter.js
+++ modules/sovrnBidAdapter.js
@@ -162,7 +162,7 @@ export const spec = {
       const urlParams = new URLSearchParams(window.location.search)
       const uuid = urlParams.get('testUuid')
 
-      let url = `https://localhost:8090/rtb/prebidResponse1/bid/openrtb25`;
+      let url = `https://fake-rtb-providers:8090/rtb/prebidResponse1/bid/openrtb25`;
       if (iv) url += `&iv=${iv}`;
 
       return {
